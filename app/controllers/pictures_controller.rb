944520b792874bbe1e022a42f20995e44f9940c6
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-11-07 20:06:35 +0000

    New pictures can be uploaded

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,20 @@
 class PicturesController < ApplicationController
 
 	def index
+		@pictures = Picture.all
 	end
+
+	def new
+		@picture = Picture.new
+	end
+
+	def create
+		@picture = Picture.create(params[:picture].permit(:feeling, :image))
+		if @picture.save
+			redirect_to pictures_path
+		else 
+			render 'new'
+		end
+	end
+
 end
--- spec/features/picture_feature_spec.rb
+++ spec/features/picture_feature_spec.rb
@@ -7,4 +7,13 @@ describe 'pictures' do
 		expect(page).to have_link 'Post picture'
 	end
 
+	it 'users can upload a photo' do
+		visit '/pictures'
+		click_link 'Post picture'
+		attach_file('Image', Rails.root + "spec/fixtures/images/bird.jpg")
+		fill_in 'Feeling', with: 'Happy'  
+		click_button 'Create Picture'
+		expect(Picture.all.count).to equal 1
+	end
+
 end
